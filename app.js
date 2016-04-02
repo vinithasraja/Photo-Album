441b02df95413026dc8c0a9c0b2007d1a5e6b1fb
vinithasraja <snellhome1@gmail.com>
2016-04-02 00:10:58 -0700

    Backbone Photo Album with ratings and display

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ var photoData = [
     id:1,
     name: 'Swan',
     image: '/Users/datascience/Documents/Hackreactor/Photo Album Project/pic1.jpg' ,
-    
+    rating: 1,
   },
   {
     id:2,
     name: 'Elephant',
     image: '/Users/datascience/Documents/Hackreactor/Photo Album Project/pic2.jpg' ,
-    
+    rating: 1,
   },
   {
     id:3,
     name: 'Dog',
     image: '/Users/datascience/Documents/Hackreactor/Photo Album Project/pic3.jpg',
+    rating: 1,
   }
 ];
   
   
 
 var Collect = new Backbone.Collection(photoData, {model: Photo});
-var AllView = new AllView({collection: Collect});
-$('body').append(AllView.$el);
+var app = new AppModel({collection: Collect});
+var appView = new AppView({model: app});
+
+$('body').append(appView.render());
+
+
