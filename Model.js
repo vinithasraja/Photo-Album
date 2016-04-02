161191cb4869f6c2bee471f8ebb486f5201a6183
vinithasraja <snellhome1@gmail.com>
2016-04-02 00:04:26 -0700

    Photomodel

--- Model.js
+++ Model.js
@@ -2,18 +2,9 @@
 
 var Photo = Backbone.Model.extend({
 
-  defaults: {
-    id: 0,
-    name: 'Larry',
-    image: 'http://www.google.com',
-    displayInfo: false
-  },
-
   Displayimage : function(){
-  value = this.get('displayInfo');
-  this.set('displayInfo', !value);
-  this.trigger('click:image',this);
   
+  this.trigger('setImage',this);
 },
 
 });
